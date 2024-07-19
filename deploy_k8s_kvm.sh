11ae998f0abe42b12b61c6fdbc03785dae4460ae
Marcin Kujawski <77984926+mariano-italiano@users.noreply.github.com>
2024-07-19 14:06:13 +0200

    Update deploy_k8s_kvm.sh

--- deploy_k8s_kvm.sh
+++ deploy_k8s_kvm.sh
@@ -20,4 +20,23 @@ else
         git clone https://github.com/mariano-italiano/kvm-k8s.git
         cd kvm-k8s/kubernetes/vagrant
         vagrant up --provider libvirt
+
+        MASTER_IP=`virsh net-dhcp-leases vagrant-libvirt | grep master |awk '{print $5}'`
+        WORKER1_IP=`virsh net-dhcp-leases vagrant-libvirt | grep worker1 |awk '{print $5}'`
+        WORKER2_IP=`virsh net-dhcp-leases vagrant-libvirt | grep worker2 |awk '{print $5}'`
+        
+        echo 
+        echo "---------------------------------------------------------"
+        echo -e "\033[0;30m\033[107mKubernetes Infrastructure details                        \033[0m"
+        echo "---------------------------------------------------------"
+        echo -e " Control plane hostname:      \033[32mmaster\033[0m"
+        echo -e " Control plane IP address:    \033[93m$MASTER_IP\033[0m"
+        echo "---------------------------------------------------------"
+        echo -e " Worker1 node hostname:       \033[32mworker1\033[0m"
+        echo -e " Worker1 node IP address:     \033[93m$WORKER1_IP\033[0m"
+        echo "---------------------------------------------------------"
+        echo -e " Worker2 node hostname:       \033[32mworker2\033[0m"
+        echo -e " Worker2 node IP address:     \033[93m$WORKER2_IP\033[0m"
+        echo "---------------------------------------------------------"
+
 fi
