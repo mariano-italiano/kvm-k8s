2ee91f664df5023ec3affed98ea726c1bc171f35
Marcin Kujawski <77984926+mariano-italiano@users.noreply.github.com>
2024-07-19 12:57:16 +0200

    Create deploy_k8s_kvm.sh

---
+++ kubernetes/vagrant/deploy_k8s_kvm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Installation script for KVM K8s cluster
+# Author: Marcin Kujawski
+# Version: 1.0
+
+VIRT_ON=`egrep -c '(vmx|svm)' /proc/cpuinfo`
+
+if [ $VIRT_ON -eq 0 ] ; then
+        echo
+        echo "Virtualization is not enabled. Please turn on Virtualization and run script again."
+        echo
+else
+        sudo apt update
+        sudo apt install -y cpu-checker qemu-kvm virt-manager libvirt-daemon-system virtinst libvirt-clients bridge-utils vagrant git
+        sudo systemctl enable --now libvirtd
+        sudo usermod -aG kvm student
+        sudo usermod -aG libvirt student
+        vagrant plugin install vagrant-libvirt
+        echo
+        git clone https://github.com/mariano-italiano/kvm-k8s.git
+        cd kubernetes/vagrant
+        vagrant up --provider libvirt
+fi
