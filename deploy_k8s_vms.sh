bbfc611092bc5c71514fd8974b551230fed61da7
Marcin Kujawski <77984926+mariano-italiano@users.noreply.github.com>
2024-08-16 10:27:13 +0200

    Update deploy_k8s_vms.sh

--- deploy_k8s_vms.sh
+++ deploy_k8s_vms.sh
@@ -14,7 +14,9 @@ else
         sudo apt install -y cpu-checker qemu-kvm virt-manager libvirt-daemon-system virtinst libvirt-clients bridge-utils sshpass vagrant git
         sudo systemctl enable --now libvirtd
         sudo usermod -aG kvm student
+        sudo usermod -aG kvm atos
         sudo usermod -aG libvirt student
+        sudo usermod -aG libvirt atos
         vagrant plugin install vagrant-libvirt
         echo
         git clone https://github.com/mariano-italiano/kvm-k8s.git
