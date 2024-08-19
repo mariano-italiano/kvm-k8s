543fc903b3f25c79b8358b47435530364788fc52
Marcin Kujawski <77984926+mariano-italiano@users.noreply.github.com>
2024-08-19 11:25:36 +0200

    Create bootstrap.sh

---
+++ vms/vagrant/bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "[TASK 8] Enable ssh password authentication"
+sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+echo 'PasswordAuthentication yes' >> /etc/ssh/sshd_config
+systemctl reload sshd
+
+echo "[TASK 9] Set root password"
+echo -e "kubeadmin\nkubeadmin" | passwd root >/dev/null 2>&1
+echo "export TERM=xterm" >> /etc/bash.bashrc
+
+echo "[TASK 10] Update /etc/hosts file"
+cat >>/etc/hosts<<EOF
+172.16.16.100   master.example.com     master
+172.16.16.101   worker1.example.com    worker1
+172.16.16.102   worker2.example.com    worker2
+EOF
