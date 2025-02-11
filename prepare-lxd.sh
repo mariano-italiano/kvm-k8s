588e8f68b314d3059d7cce773f8ce1416bd6b42a
Marcin Kujawski <77984926+mariano-italiano@users.noreply.github.com>
2025-02-11 21:39:25 +0100

    Update prepare-lxd.sh

--- prepare-lxd.sh
+++ prepare-lxd.sh
@@ -3,11 +3,11 @@
 sudo virsh start master
 sudo virsh start node1
 sudo virsh start node2
-sleep 30
+sleep 60
 ssh-keygen -q -t rsa -N '' -f /home/$USER/.ssh/id_rsa
-sshpass -p "PASSWORD" ssh-copy-id -o StrictHostKeyChecking=no student@master
-sshpass -p "PASSWORD" ssh-copy-id -o StrictHostKeyChecking=no student@node1
-sshpass -p "PASSWORD" ssh-copy-id -o StrictHostKeyChecking=no student@node2
+sshpass -p "altkom" ssh-copy-id -o StrictHostKeyChecking=no student@master
+sshpass -p "altkom" ssh-copy-id -o StrictHostKeyChecking=no student@node1
+sshpass -p "altkom" ssh-copy-id -o StrictHostKeyChecking=no student@node2
 
 for i in master; do ssh student@$i "sudo -S apt-get remove kubelet kubeadm kubectl -y; sudo rm /etc/apt/sources.list.d/kubernetes.list; sudo rm -rf /home/student/.kube; sudo kubeadm reset -f"; done 
 for i in node1 node2; do ssh student@$i "sudo -S kubeadm reset -f; sudo rm /etc/apt/sources.list.d/kubernetes.list"; done
